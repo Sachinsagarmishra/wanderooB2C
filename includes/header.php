3f588c4a17a9761d255e5574cf34535de90bd23a
Sachin Sagar <sachinsagar@Sachins-MacBook.local>
2026-05-26 00:30:47 +0530

    Remove Blogs link from header navigation menu

--- includes/header.php
+++ includes/header.php
@@ -31,7 +31,6 @@ $pageDesc = isset($pageDesc) ? $pageDesc : "A premium PHP-based website with cus
                 <a href="#">About Us</a>
                 <a href="#">Destinations <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L5 5L9 1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
                 <a href="#">Contact Us</a>
-                <a href="#">Blogs</a>
             </nav>
 
             <div class="header-actions">
